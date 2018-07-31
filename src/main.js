2f02613f4ad0e5690845e6296af6c008007cde9b
AnaeyGuillen <anaey_gu@hotmail.com>
2018-07-31 12:51:30 -0500

    Bajando login

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//Menu Materialize//
+$(document).ready(function () {
+  $(".btn").sideNav({
+    menuWidth: 300,
+    edge: 'left'
+  });
+});
+ //Menu Materialize//
+ $(document).ready(function(){
+     $('select').formSelect();
+   });
